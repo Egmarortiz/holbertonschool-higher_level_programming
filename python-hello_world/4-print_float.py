a32fe586924ca4e9af08560ea61ccd98b8b40898
Egmarortiz <egmarocasio9@gmail.com>
2025-05-06 13:40:02 -0400

    float

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -5,8 +5,8 @@ Basic Python program boilerplate.
 
 
 def main():
-    number = 3.14
-    print(f"Float: {number}")
+    number = 3.14159
+    print(f"Float: {number:.2f}")
 
 
 if __name__ == "__main__":
