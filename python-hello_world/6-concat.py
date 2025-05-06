f0750e5d650fd06f0c606b1354e9940be72028a5
Egmarortiz <egmarocasio9@gmail.com>
2025-05-06 14:22:14 -0400

    fix

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-Basic Python program.
-"""
-
-def main():
-    str1 = "Holberton"
-    str2 = "School"
-    print(f"Welcome to {str1} {str2}!")
-
-if __name__ == "__main__":
-    main()
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")
