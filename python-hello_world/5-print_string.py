5aefd29e481bf0569eea0ea24fa7501dd0e1781e
Egmarortiz <egmarocasio9@gmail.com>
2025-05-06 13:48:42 -0400

    fix

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Basic Python program boilerplate.
+"""
+
+def main():
+    str = "Holberton School"
+    print(str * 3)
+    print(str[0:9])
+
+if __name__ == "__main__":
+    main()
