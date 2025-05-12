aa8b174386dfb5c52bea7417f46a613479e8334d
Egmarortiz <egmarocasio9@gmail.com>
2025-05-12 16:34:25 -0400

    fix

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
-#list(map(chr, [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
-
-#s = ['1', '2', '3', '4']
-#res = map(int, s)
-#print(list(res))
-
 for abc in range(97, 123):
-    if abc >= 65 or abc <= 91:
-        print(f"{chr(abc)}")
+        print(f"{chr(abc)}", sep= '', end = '')
