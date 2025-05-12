ffc465efe51e3555e4c79137310f73f1c9ec5ab5
Egmarortiz <egmarocasio9@gmail.com>
2025-05-12 16:57:19 -0400

    abc

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,5 @@
 #for abc in range(97, 123):
        # print(f"{chr(abc)}", sep= '', end = '')
 
-print(f"{''.join(chr(i) for i in range(97,123))}", end='')
+print("{}".format(''.join(chr(i) for i in range(97,123))), end='')
+
