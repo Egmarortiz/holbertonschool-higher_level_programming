fd61be8e0e3e0c0eb5e3d5653ee3e7b50a173d5a
Egmarortiz <egmarocasio9@gmail.com>
2025-05-13 13:15:04 -0400

    fix

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("{}".format('\n'.join(f"{dec:d} = {hex(dec)}" for dec in range(99))), end= '\n')
+print("{}".format('\n'.join(f"{dec:d} = {hex(dec)}" for dec in range(99))),
+      end='\n')
