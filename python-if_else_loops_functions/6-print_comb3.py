5b0e267c573b8fc32f0cea5ac8096ba099c17320
Egmarortiz <egmarocasio9@gmail.com>
2025-05-14 15:21:36 -0400

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 print(
-        "{}".format(
-    ", ".join(
-        f"{i}{j}"
-        for i in range(9)
-        for j in range(i + 1, 10)
-    )
-), end='\n')
+    "{}".format(
+        ", ".join(
+            f"{i}{j}"
+            for i in range(9)
+            for j in range(i + 1, 10)
+        )
+    ),
+    end='\n'
+)
+
