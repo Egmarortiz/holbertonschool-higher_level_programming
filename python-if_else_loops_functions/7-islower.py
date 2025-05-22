4d0a43f70a714d0e10e09e200c9063f852dd8f69
Egmarortiz <egmarocasio9@gmail.com>
2025-05-22 16:55:46 -0400

    fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(c):
 
-  #  code = ord(c)
+    #  code = ord(c)
 
    # if code < 97 or code > 122:
     #    print(f"{chr(code)} is upper")
@@ -12,11 +12,9 @@ def islower(c):
     if not isinstance(c, str) or len(c) != 1:
         return False
 
-    code = ord(c)
+    return c.islower()
 
-    return 97 <= code <= 122
-
-    if __name__ == "__main__":
-        tests = ['a', 'Z', 'm', '1', '', 'ab']
-        for ch in tests:
-            print(f"{repr(ch)} -> {islower(ch)}")
+if __name__ == "__main__":
+    tests = ['a', 'Z', 'm', '1', '', 'ab']
+    for ch in tests:
+        print(f"{repr(ch)} -> {islower(ch)}")
