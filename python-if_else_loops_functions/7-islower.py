684a59a0aa9b7b89c80e0e91daa9d4ec723cf186
Egmarortiz <egmarocasio9@gmail.com>
2025-05-22 18:32:47 -0400

    fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    """
-    Returns True if character c is a lowercase letter (a–z), False otherwise.
-    """
-
-    if not isinstance(c, str) or len(c) != 1:
-        return False
-
-    return 'a' <= c <= 'z'
+    return ord(c) >= 97 and ord(c) <= 122
