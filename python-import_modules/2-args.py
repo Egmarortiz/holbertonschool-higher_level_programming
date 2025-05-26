a8fce59d6d40d704dd5ba5a88142c9f1b37ae2c4
Egmarortiz <egmarocasio9@gmail.com>
2025-05-26 16:27:10 -0400

    fix

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# File demo.py
+import sys
+
+def main():
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for index, arg in enumerate(args, start=1):
+        print(f"{index}: {arg}")
+
+if __name__ == "__main__":
+    main()
