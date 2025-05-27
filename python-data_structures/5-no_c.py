ca682737307e0d7acc4c52c293fbf8ec99fa673c
Egmarortiz <egmarocasio9@gmail.com>
2025-05-27 12:01:41 -0400

    fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-
-    number = [ord(x) for x in my_string
-              if my_string == 43 or my_string == 63
-              continue]
-
-    reverse = ''.join(chr(x) for x in number)
-
-    return reverse
+    filtered = [x for x in my_string if x not in ('c', 'C')]
+    return "".join(filtered)
