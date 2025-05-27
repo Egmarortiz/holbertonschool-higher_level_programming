dc375fd14a786b5e3ff33f2dd5d6929ee8a9ca7c
Egmarortiz <egmarocasio9@gmail.com>
2025-05-26 23:57:15 -0400

    fix

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+
+    num = my_list=[]
+
+    if num == []:
+        print("list cannot be empty")
+
+    for x in num:
+        print("{}".format(x))
