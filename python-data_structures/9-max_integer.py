0a88241fe64c261a45dd3fa9d52287b36a64b0bb
Egmarortiz <egmarocasio9@gmail.com>
2025-05-29 15:03:22 -0400

    fix

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max_val = lst[0]  # Start by assuming the first item is the largest
+    for num in lst:
+        if num > max_val:
+            max_val = num  # Update max_val if a bigger number is found
+    return max_val
