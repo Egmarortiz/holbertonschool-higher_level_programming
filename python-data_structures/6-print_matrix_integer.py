55ae0d03d65dfdcd366bd4888c58464672680fe8
Egmarortiz <egmarocasio9@gmail.com>
2025-05-29 03:03:07 -0400

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of integers, one row per line, with each number formatted via str.format()."""
     for row in matrix:
         for i, item in enumerate(row):
             # use integer-specific formatting and no explicit casting
