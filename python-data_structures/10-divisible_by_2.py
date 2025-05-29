30b353327a3588881801d8d8bffd74f8c2781bda
Egmarortiz <egmarocasio9@gmail.com>
2025-05-29 15:40:28 -0400

    fix

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,8 +4,6 @@ def divisible_by_2(my_list=[]):
 
     new_list = []
 
-    for div in my_list:
-        if div % 2 == 0:
-            return new_list.append(True)
-        else:
-            return new_list.append(False)
+    for div in my_list:\
+        new.list.append(div % 2 == 0)
+    return new_list
