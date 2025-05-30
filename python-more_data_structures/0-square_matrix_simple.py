f40d251be77459c378c76c406c092080e38d06a2
Egmarortiz <egmarocasio9@gmail.com>
2025-05-30 13:36:49 -0400

    fix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,12 +2,9 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-
     for row in matrix:
-        for i, item in enumerate(row):
-            new_matrix.append(item ** 2)
+        new_row = []
+        for item in row:
+            new_row.append(item ** 2)
+        new_matrix.append(new_row)
     return new_matrix
-    return matrix
-
-
-
