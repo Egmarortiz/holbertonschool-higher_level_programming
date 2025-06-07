b44c04de9dc93012e842b07011a8a03af3af2302
Egmarortiz <egmarocasio9@gmail.com>
2025-06-06 23:43:08 -0400

    fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,9 @@ Prints: My name is <first_name> <last_name>.
 """
 
 def say_my_name(first_name, last_name=""):
-"""Print My name is <first_name> <last_name>."""
+
+    """Print My name is <first_name> <last_name>."""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
