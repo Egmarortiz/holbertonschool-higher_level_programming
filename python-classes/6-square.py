4434c7b9838d4271878c3744a48866892d3f0b1e
Egmarortiz <egmarocasio9@gmail.com>
2025-06-13 18:41:33 -0400

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Module 3-square: defines a Square."""
         self.__size = size
-        self._position = position
+        self.__position = position
 
     @property
     def size(self):
@@ -17,9 +17,9 @@ class Square:
     @size.getter
     def size(self, value):
         """Module 3-square: defines a Square."""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an int")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -32,8 +32,8 @@ class Square:
     def position(self, value):
         """Module 3-square: defines a Square."""
         if (
-            not isinstance(position, tuple)
-            or len(position) != 2
+            not isinstance(value, tuple)
+            or len(value) != 2
             or not all (isinstance(n, int) for n in value) or
             not all(n >= 0 for n in value)
         ):
@@ -49,6 +49,7 @@ class Square:
         if self.__size == 0:
             print()
             return
+
         for _ in range(self.__position[1]):
             print()
 
