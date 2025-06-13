7795ce82d277a47bd3ca2a033a8f5d83938b0349
Egmarortiz <egmarocasio9@gmail.com>
2025-06-13 19:25:36 -0400

    fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,8 +4,8 @@
 class Rectangle:
     """Module defines a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -15,9 +15,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Module defines a rectangle"""
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -29,9 +29,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Module defines a rectangle"""
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
