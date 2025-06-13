7b6dd71b936dce9acbb12b8c2d14fcf5e5cb82b2
Egmarortiz <egmarocasio9@gmail.com>
2025-06-13 12:07:30 -0400

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,17 +7,21 @@ class Square:
     def __init__(self, size=0):
         """docustring"""
         self.__size = size
+
     @property
     def size(self):
         """docustring"""
         return self.__size
+    
     @size.setter
     def size(self, value):
         """docustring"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         """docustring"""
         return self.__size ** 2
