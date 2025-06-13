53f8e5905aaf3a92e424bced27542e202654d103
Egmarortiz <egmarocasio9@gmail.com>
2025-06-13 10:43:08 -0400

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """ Module contains a simple square"""
 
-
 class Square:
-    """simple class Square"""
-    def __init__(self, size):
+    def __inti__(self, size=0):
         self.__size = size
-        """simple class Square"""
-        if not isinstance(size, (int, float)):
-            raise TypeError("size must be a number")
-        """simple class Square"""
-        if size < 0:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size <= 0:
             raise ValueError("size must be >= 0")
