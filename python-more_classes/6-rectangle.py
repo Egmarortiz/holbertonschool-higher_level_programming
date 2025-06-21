a84f905b008f7f07655332501feba7ce905222fb
Egmarortiz <egmarocasio9@gmail.com>
2025-06-20 23:59:04 -0400

    fix

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+#module 6
+
+"""Module deines a rectangle"""
+
+
+class Rectangle:
+    number_of_instances = 0
+    """Module defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Module defines a rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Module defines a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Module defines a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Module defines a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Module deines a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Module deines a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Module deines a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        lines = []
+        for _ in range(self.__height):
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
+
+    def __repr__(self):
+        """Module defines a rectangle"""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Module defines a rectangle"""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle…")
+
---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+#module 8
+
+"""Module deines a rectangle"""
+
+
+class Rectangle:
+    number_of_instances = 0
+    print_symbol = "#"
+
+    """Module defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Module defines a rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Module defines a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Module defines a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Module defines a rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Module deines a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Module deines a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Module deines a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        sym = str(self.print_symbol)
+        lines = [sym * self.__width for _ in range(self.__height)]
+        return "\n".join(lines)
+
+    def __repr__(self):
+        """Module defines a rectangle"""
+
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Module defines a rectangle"""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle…")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Module returns a rectangle""" 
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area >= rect_2.area:
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Module returns rectangle"""
+        return cls(size, size) 
