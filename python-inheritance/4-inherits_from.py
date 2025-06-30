1aa75ef11cf5010a4f2f7b2dcd8231e0752baaf5
Egmarortiz <egmarocasio9@gmail.com>
2025-06-29 20:16:46 -0400

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,6 @@
 
 def inherits_from(obj, a_class):
     """does it inherit"""
-    if type(obj) not a_class
-        return issubclass(type(obj), a_class)
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
