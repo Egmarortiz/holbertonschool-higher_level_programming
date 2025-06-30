4700ee8fe76d8e17415c39cf116dbe51916c139d
Egmarortiz <egmarocasio9@gmail.com>
2025-06-29 21:36:47 -0400

    fix

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module defines a rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+    def __init__(self, width, height):
+        """Initaialize and validate parameters"""
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        print(f"[Rectangle] {self.__width}/{self.__height}")
+
+
