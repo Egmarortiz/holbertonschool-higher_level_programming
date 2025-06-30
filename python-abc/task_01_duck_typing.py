33d6304dcafe2c94db5af0c67c9a2b06a82542e3
Egmarortiz <egmarocasio9@gmail.com>
2025-06-30 12:39:19 -0400

    fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -31,7 +31,7 @@ class Rectangle(Shape):
         return self.width * self.height
 
     def perimeter(self):
-        return 2 * self.width + self.height
+        return 2 * (self.width + self.height)
 
 def shape_info(arg1):
     arg1.area()
