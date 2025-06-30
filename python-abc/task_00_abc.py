655a9f0804932ce3aecf248fb6a29024a45dd89e
Egmarortiz <egmarocasio9@gmail.com>
2025-06-30 12:05:04 -0400

    fix

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Abstract animal class"""
+
+
+from abc import ABC
+
+
+class Animal(ABC):
+    """Animal class"""
+    @abstractmethod
+    def sound(self):
+        """Abstract sound method"""
+        pass
+
+class Dog(Animal):
+    """Dog Class"""
+    def sound(self):
+        """Inherited sound method"""
+        print("Bark")
+
+class Cat(Animal):
+    """Cat class"""
+    def sound(self):
+        """Inherited sound method"""
+        print("Meow")
