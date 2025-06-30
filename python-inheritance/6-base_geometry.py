ce8e6524519ecd51480577ee50339855fc82cb1e
Egmarortiz <egmarocasio9@gmail.com>
2025-06-29 20:23:06 -0400

    fix

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""raising exception"""
+
+
+class BaseGeometry:
+    """Raising excecption"""
+    def area(self):
+        """Raising exception"""
+        raise Exception("area() is not implemented")
