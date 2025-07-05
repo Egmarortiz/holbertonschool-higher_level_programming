eb959714563a39c020bbddab7e289bf544479d8d
Egmarortiz <egmarocasio9@gmail.com>
2025-07-05 13:50:42 -0400

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,6 +2,7 @@
 """Writes text to a file"""
 
 def write_file(filename="", text=""):
+    """Writes text in a file"""
     with open(filename, 'w', encoding='utf-8') as f:
         count = f.write(text)
     retuen count
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """Appends text to a file"""
     with open(filename, 'a', encoding='utf-8') as f:
         count = f.write(text)
     return count
