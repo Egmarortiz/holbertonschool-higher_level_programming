be26cc8e4b228b291406ec64ae4d9359fc33a724
Egmarortiz <egmarocasio9@gmail.com>
2025-07-07 00:54:58 -0400

    fix

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""Basic encoding and decoding"""
+
 
 import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """Encoding data into file"""
     with open(filename, 'w', endcoding='utf-8') as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
+    """Decoding data into file"""
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
