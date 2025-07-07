d1110bb030050189780893e86d6fef6c691c2074
Egmarortiz <egmarocasio9@gmail.com>
2025-07-06 22:36:41 -0400

    fxi

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module returns class dict"""
+
 
 class Student:
+    """Return class dict"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
