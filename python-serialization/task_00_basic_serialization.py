59252c298be8b852e3767c52c2f1fe28bf6e796f
Egmarortiz <egmarocasio9@gmail.com>
2025-07-07 00:53:01 -0400

    fix

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,7 +5,7 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, 'w', endcoding='utf-8') as f:
-        return json.dump(data, f)
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
     with open(filename, 'r', encoding='utf-8') as f:
