0048f186cc497d6199551b6f2d59a2cedcf28bd0
Egmarortiz <egmarocasio9@gmail.com>
2025-07-08 21:03:46 -0400

    fix

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Implementing pickle module"""
+import pickle
+
+class CustomObject:
+    """Implementing Pickle Module"""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+    try:
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+        return True
+    except (FileNotFoundError, pickle.PickleError, Exception):
+        return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as f:
+                loaded = pickle.load(f)
+            return loaded
+        except (FileNotFoundError, pickle.pickleError, Exception):
+            return None
