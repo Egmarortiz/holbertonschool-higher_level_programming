e5a83a38e79f99fa06b047c009e5d134f402cfd2
Egmarortiz <egmarocasio9@gmail.com>
2025-07-22 22:23:21 -0400

    fix

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,11 +1,13 @@
--- STATES TABLE
-CREATE DATABASE
-IF NOT EXISTS hbtn_0d_usa;
+-- create_hbtn_0d_usa.sql
 
-USE hbnt_0d_usa;
+-- 1) Create the database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE
-IF NOT EXISTS hbnt_0d_usa.states (
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  );
+-- 2) Switch into that database
+USE hbtn_0d_usa;
+
+-- 3) Create the states table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS states (
+  id   INT              NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256)     NOT NULL
+);
