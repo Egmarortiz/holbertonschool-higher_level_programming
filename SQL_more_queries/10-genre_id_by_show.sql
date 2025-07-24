818028cacf7502e077e6f9a9f714e94a18c09185
Egmarortiz <egmarocasio9@gmail.com>
2025-07-24 15:35:15 -0400

    genre by id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,6 @@
--- Genre ID by show
-SELECT
-  tv_shows.title,
-  tv_show_genres.genre_id
+-- Lists all shows that have at least one genre
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
-ON tv.shows.id = tv_show_genres.show_id
-ORDER BY tv_show_title ASC, tv_show_genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
