f6b1cd551b533eb893e08c85d54cbc632d18797e
Egmarortiz <egmarocasio9@gmail.com>
2025-07-27 17:41:01 -0400

    using format for sys input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Takes in an argument and displays all values in the states table
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    search_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(search_name)
+    cursor.execute(query)
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Print results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
+
